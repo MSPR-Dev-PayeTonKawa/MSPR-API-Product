4eeef9e0d82dc76f668cf67ad20122ed19554f85
Benjamin AMAND <benjamin.amand@supermarchesmatch.fr>
2025-06-13 11:46:11 +0200

    First commit

---
+++ src/test/java/com/payetonkawa/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payetonkawa.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
